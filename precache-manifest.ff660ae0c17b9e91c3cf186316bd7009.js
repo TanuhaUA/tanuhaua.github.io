2aa93a51abc210c383cb5a79102a18293a38c5ff
tanuha <tanuha@zeoalliance.com>
2019-02-23 23:41:57 +0200

    mate hackhathon

---
+++ precache-manifest.ff660ae0c17b9e91c3cf186316bd7009.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "8536ed1da0daf7476ccd01951a6eb0b1",
+    "url": "/static/media/home.8536ed1d.jpg"
+  },
+  {
+    "revision": "8ef39e783bb6546ab9411b58e7300f6d",
+    "url": "/static/media/Layer4.8ef39e78.jpg"
+  },
+  {
+    "revision": "90e1d3559ac52f7f0f77a86e1bfd632d",
+    "url": "/static/media/Lato.90e1d355.ttf"
+  },
+  {
+    "revision": "c3be8cbba61a6e6d883ba7d3b404f519",
+    "url": "/static/media/HelveticaNeueBold.c3be8cbb.ttf"
+  },
+  {
+    "revision": "31d1ca77eca12c27cb7d61666db19be1",
+    "url": "/static/media/Existence-Light.31d1ca77.otf"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "4f2da9e89f8fc5fef4fe",
+    "url": "/static/js/main.4f2da9e8.chunk.js"
+  },
+  {
+    "revision": "a66add37c75d96f43f08",
+    "url": "/static/js/2.a66add37.chunk.js"
+  },
+  {
+    "revision": "4f2da9e89f8fc5fef4fe",
+    "url": "/static/css/main.eea06523.chunk.css"
+  },
+  {
+    "revision": "8183d287d7cfd3bc6dd84adc0176bdc5",
+    "url": "/index.html"
+  }
+];
